b6f0847e4fd94cd34195a53faac2c59aa4338069
Himanshu5296 <himanshu.singh5296@gmail.com>
2022-06-18 18:59:14 +0530

    PAYMENT PAGE COMPLETE

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 
 import './App.css';
+import Payment from './Components/Payment/Payment';
 //import Emptycart from './Components/Empty/Emptycart';
-import ProductCart from './Components/Non Empty Cart/ProductCart';
+//import ProductCart from './Components/Non Empty Cart/ProductCart';
 
 function App() {
   return (
     <div className="App">
        {/* <Emptycart/> */}
-       <ProductCart/>
+       {/* <ProductCart/> */}
+       <Payment/>
     </div>
   );
 }
